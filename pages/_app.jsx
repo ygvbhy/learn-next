2a15136cc7ccac13466d274873d8c236b17903ab
ygvbhy <ygvbhy@naver.com>
2024-06-25 21:22:09 +0900

    Layout 에 대한 개념 이해 및 적용 쓸모없는 기본 css 내용 삭제

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,10 @@
 import '@/styles/globals.css';
-import Link from 'next/link';
+import Layout from './layouts/Layout';
 
 export default function App({ Component, pageProps }) {
 	return (
-		<div>
-			<nav>
-				<Link href="/home">home</Link> | <Link href="/login">login</Link>
-			</nav>
+		<Layout>
 			<Component {...pageProps} />
-		</div>
+		</Layout>
 	);
 }
