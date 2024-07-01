10ad8b988d9606ffa45baf1f48416a72eb07bf32
ygvbhy <ygvbhy@naver.com>
2024-07-01 22:19:06 +0900

    axios 설치 및 간단한 get 데이터 받아오기

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,17 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function ProductPage() {
+	const [products, setProducts] = useState();
+
+	useEffect(() => {
+		axios.get('http://localhost:4000/products').then(response => {
+			setProducts(response.data);
+		});
+	}, []);
+
+	console.log(products);
+
 	return <div style={{ color: 'blue' }}>hello next</div>;
 }
 
