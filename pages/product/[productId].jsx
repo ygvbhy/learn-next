18d6613f8d425d633ea80a13bf9236538511929f
ygvbhy <ygvbhy@naver.com>
2024-07-02 21:54:49 +0900

    props 실습

--- pages/product/[productId].jsx
+++ pages/product/[productId].jsx
@@ -1,7 +1,15 @@
+import ProductHeader from '@/components/ProductHeader';
 import React from 'react';
 
 export default function ProductDetailPage({ message }) {
-	return <div>ProductDetailPage {message} </div>;
+	const headerTitle = '상품 상세 페이지';
+
+	return (
+		<>
+			<ProductHeader title={headerTitle} />
+			<div>ProductDetailPage {message} </div>
+		</>
+	);
 }
 
 export async function getServerSideProps(context) {
