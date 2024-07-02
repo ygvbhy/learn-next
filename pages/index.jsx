7269ec15fbf54a3eb9008224cd232e0cb09901a1
ygvbhy <ygvbhy@naver.com>
2024-07-02 21:48:51 +0900

    props 개념 정리

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,14 @@
+import ProductHeader from '@/components/ProductHeader';
 import ProductList from '@/components/ProductList';
 
 function ProductPage() {
+	// 이렇게 넣어도 되고 그냥 변경되지 않는 값이라면 string 값
+	// 그대로 넣어도 됨
+	const headerTitle = '상품 목록 페이지';
+
 	return (
 		<div>
-			<h1>상품 목록 페이지</h1>
+			<ProductHeader title={headerTitle} />
 			<ProductList />
 		</div>
 	);
