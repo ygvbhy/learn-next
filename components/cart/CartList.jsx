2da3d192e14f607d6e4f77adced4bcb39f8a6ba8
ygvbhy <ygvbhy@naver.com>
2024-07-07 15:01:48 +0900

    장바구니 총 수량 및 가격 구현

--- components/cart/CartList.jsx
+++ components/cart/CartList.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 import styles from './CartList.module.css';
 
 export default function CartList({ carts }) {
+	const totalPrice = carts.reduce((acc, cur) => {
+		return acc + +cur.price;
+	}, 0);
+
 	return (
 		<>
 			<div>
@@ -28,7 +32,7 @@ export default function CartList({ carts }) {
 				</ul>
 			</div>
 			<div>
-				<p>총 가격 : </p>
+				<p>총 가격 : {totalPrice}</p>
 				<p>총 수량 : {carts.length}</p>
 			</div>
 		</>
