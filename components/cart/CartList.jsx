6c8eda5485c728646cdebae0f8467e623735d8e0
ygvbhy <ygvbhy@naver.com>
2024-07-07 14:53:32 +0900

    간단한 장바구니 ui 구성

--- components/cart/CartList.jsx
+++ components/cart/CartList.jsx
@@ -1,4 +1,6 @@
+import Image from 'next/image';
 import React from 'react';
+import styles from './CartList.module.css';
 
 export default function CartList({ carts }) {
 	return (
@@ -6,13 +8,28 @@ export default function CartList({ carts }) {
 			<div>
 				<ul>
 					{carts.map(cart => {
-						return <li key={cart.id}>{cart.name}</li>;
+						return (
+							<li key={cart.id} className={styles.item}>
+								<div>
+									<Image
+										src={cart.imageUrl}
+										width={75}
+										height={75}
+										alt={cart.name}
+									></Image>
+								</div>
+								<div className={styles.dexcription}>
+									<div>{cart.name}</div>
+									<div>${cart.price}</div>
+								</div>
+							</li>
+						);
 					})}
 				</ul>
 			</div>
 			<div>
 				<p>총 가격 : </p>
-				<p>총 수량 : </p>
+				<p>총 수량 : {carts.length}</p>
 			</div>
 		</>
 	);
